db29fd8c6fca74a5a4dbb0f4bb314d0ad1589f43
ghanimmukhtar <ghanimmukhtar@gmail.com>
2017-08-03 17:33:20 +0200

    working version with kinect v2, baxter

--- src/test_camera_robot_calibration.cpp
+++ src/test_camera_robot_calibration.cpp
@@ -22,18 +22,21 @@ int main(int argc, char **argv)
 
     bool result = my_calibrator->solve_for_transformation_matrix();
     if(result){
-    ROS_INFO_STREAM("Quaternion angles are: " << my_calibrator->get_global_parameters().get_transformation_quaternion());
-    ROS_WARN("*************************************");
-    ROS_INFO_STREAM("RPY angles are: " << my_calibrator->get_global_parameters().get_transformation_RPY());
-    ROS_WARN("*************************************");
+        ROS_INFO_STREAM("Position is: " << my_calibrator->get_global_parameters().get_transformation_position());
+        ROS_INFO_STREAM("Quaternion angles are: " << my_calibrator->get_global_parameters().get_transformation_quaternion_vector());
+        ROS_WARN("*************************************");
+        ROS_INFO_STREAM("RPY angles are: " << my_calibrator->get_global_parameters().get_transformation_RPY());
+        ROS_WARN("*************************************");
 
-    Eigen::Matrix4d trans_matrix = my_calibrator->get_global_parameters().get_transformation_matrix();
+        Eigen::Matrix4d trans_matrix = my_calibrator->get_global_parameters().get_transformation_matrix();
 
-    ROS_INFO_STREAM("Transformation matrix is: " << trans_matrix(0, 0) << ", " << trans_matrix(0, 1) << ", " << trans_matrix(0, 2) << ", " << trans_matrix(0, 3) << ", " <<
-                    trans_matrix(1, 0) << ", " << trans_matrix(1, 1) << ", " << trans_matrix(1, 2) << ", " << trans_matrix(1, 3) << ", " <<
-                    trans_matrix(2, 0) << ", " << trans_matrix(2, 1) << ", " << trans_matrix(2, 2) << ", " << trans_matrix(2, 3) << ", " <<
-                    trans_matrix(3, 0) << ", " << trans_matrix(3, 1) << ", " << trans_matrix(3, 2) << ", " << trans_matrix(3, 3));
-    ROS_WARN("*************************************");
+        ROS_INFO_STREAM("Transformation matrix is: " << "\n" <<
+                        trans_matrix(0, 0) << ", " << trans_matrix(0, 1) << ", " << trans_matrix(0, 2) << ", " << trans_matrix(0, 3) << ",\n " <<
+                        trans_matrix(1, 0) << ", " << trans_matrix(1, 1) << ", " << trans_matrix(1, 2) << ", " << trans_matrix(1, 3) << ",\n " <<
+                        trans_matrix(2, 0) << ", " << trans_matrix(2, 1) << ", " << trans_matrix(2, 2) << ", " << trans_matrix(2, 3) << ",\n " <<
+                        trans_matrix(3, 0) << ", " << trans_matrix(3, 1) << ", " << trans_matrix(3, 2) << ", " << trans_matrix(3, 3));
+        ROS_WARN("*************************************");
+        return 0;
     }
     else
         ROS_WARN("No calibration results, redo it please !!");
